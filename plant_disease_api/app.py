3ac8d7ab788d8e5191617f2ddba3aa10f0a1bdec
Curius Krish <curiuskrish@gmail.com>
2025-06-20 10:47:18 +0530

    fix: bind Flask app to correct host and port for Render

--- plant_disease_api/app.py
+++ plant_disease_api/app.py
@@ -66,6 +66,7 @@ def predict():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))  # Default to 10000 if not set
     app.run(host="0.0.0.0", port=port)
+
 
